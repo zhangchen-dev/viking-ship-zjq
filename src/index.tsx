94f58d5b18eef6d01baad3ee206d8ae923332d8f
张佳琪(10070263) <zhangjq-l@glodon.com>
2023-03-06 16:09:25 +0800

    fix: 1

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+//大法师打发斯蒂芬
+const a=1,b=2;
+let c=a+b
 root.render(
   <React.StrictMode>
     <App />
