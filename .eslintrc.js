3c18f9a28f62b7075668fb563025be2d08eb264d
张佳琪(10070263) <zhangjq-l@glodon.com>
2023-03-23 14:38:48 +0800

    feat: 继承prettier依赖规则到eslint中

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Author: 张佳琪(10070263) zhangjq-l@glodon.com
  * @Date: 2023-03-07 09:01:40
  * @LastEditors: 张佳琪(10070263) zhangjq-l@glodon.com
- * @LastEditTime: 2023-03-23 10:28:53
+ * @LastEditTime: 2023-03-23 14:35:47
  * @FilePath: \viking-ship\.eslintrc.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
